90018ce5da6069447dd4e1e70093b4b465ddc690
Toshihiro <adigungodwin2@gmail.com>
2023-02-06 17:00:07 +0100

    configured accounts.admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+class AccountAdmin(UserAdmin):
+    readonly_fields = ('date_joined', 'last_login')
+    filter_horizontal = ()
+    list_filter = ()
+admin.site.register(User, AccountAdmin)
