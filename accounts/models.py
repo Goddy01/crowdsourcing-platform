30e3d89b9f6fc260ffe69a6e6666556bf0b8bf55
Toshihiro <adigungodwin2@gmail.com>
2023-02-06 16:27:59 +0100

    added models for users in accounts.models

---
+++ accounts/models.py
@@ -0,0 +1,159 @@
+from __future__ import unicode_literals
+from enum import unique
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.core.validators import RegexValidator
+from phonenumber_field.modelfields import PhoneNumberField
+from django.conf import settings
+# Create your models here.
+
+
+class AccountManager(BaseUserManager):
+    """Creates and saves a user with the given details"""
+    def create_user(self, email, username, first_name, last_name, middle_name, phone_num, password):
+        if not email:
+            raise ValueError("The user must provide an email")
+        if not username:
+            raise ValueError("The user must provide a username")
+        if not first_name:
+            raise ValueError("The user must provide their first_name")
+        if not last_name:
+            raise ValueError("The user must provide their last_name")
+        if not middle_name:
+            raise ValueError("The user must provide their middle_name")
+        if not phone_num:
+            raise ValueError("The user must provide their phone number")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            middle_name=middle_name,
+            phone_num=phone_num
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, middle_name, phone_num, password):
+        """Creates and saves a superuser with the given details"""
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            middle_name=middle_name,
+            phone_num=phone_num,
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+
+def upload_location_pfp(instance, filename):
+    return f'pfps/{str(instance.username)}/-{filename}'
+
+def upload_location_id_card(instance, filename):
+    return f'id_cards/{str(instance.username)}/-{filename}'
+
+class User(AbstractBaseUser):
+    last_name =                     models.CharField(max_length=256)
+    first_name =                    models.CharField(max_length=256)
+    middle_name =                   models.CharField(max_length=256)
+    username =                      models.CharField(max_length=256, unique=True, blank=False)
+    email =                         models.EmailField(max_length=128, unique=True, blank=False)
+    pfp =                           models.ImageField(upload_to=upload_location_pfp, blank=False)
+    id_card =                       models.ImageField(upload_to=upload_location_id_card, blank=False)
+    city =                          models.CharField(max_length=128, blank=False)
+    state =                         models.CharField(max_length=128, blank=False)
+    country =                       models.CharField(max_length=128, blank=False)
+    address =                       models.CharField(max_length=128, blank=False)
+    phone_num =                     PhoneNumberField(null=False, blank=True, verbose_name="Phone Number")
+    date_joined =                   models.DateTimeField(auto_now_add=True)
+    last_login =                    models.DateTimeField(auto_now=True)
+    is_admin =                      models.BooleanField(default=False)
+    is_staff =                      models.BooleanField(default=False)
+    is_active =                     models.BooleanField(default=True)
+    is_superuser =                  models.BooleanField(default=False)
+    is_verified =                   models.BooleanField(default=False)
+    # is_vendor               = models.BooleanField(default=False)
+    # is_customer             = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        '''Returns the first_name plus the last_name, with a space in between.'''
+        full_name = '%s, %s %s' % (self.last_name, self.first_name, self.middle_name)
+        return full_name.strip()
+
+    def has_perm(self, perm, obj=None):
+        """Checks if the user has any permissions"""
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        """Checks if the user has permission to view the app 'app_label'"""
+        return True
+
+    class Types(models.TextChoices):
+        MODERATOR = "VENDOR", "Vendor"
+        INNOVATOR = "INNOVATOR", "Innovator"
+        INVESTOR = "INVESTOR", "Investor"
+
+    type =                          models.CharField(max_length=50, default=Types.INNOVATOR, choices=Types.choices, verbose_name='Type')
+
+class ModeratorManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        # When the all() method of this custom manager is called, I will get all users that are moderators.
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.MODERATOR)
+
+class InnovatorManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        # When the all() method of this custom manager is called, I will get all users that are innovators.
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.INNOVATOR)
+
+class InvestorManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        # When the all() method of this custom manager is called, I will get all users that are investors.
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.INVESTOR)
+
+class Moderator(settings.AUTH_USER_MODEL):
+    objects = ModeratorManager()
+    class Meta:
+        proxy = True
+        
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = settings.AUTH_USER_MODEL.Types.MODERATOR
+        return super().save(*args, **kwargs)
+
+class Innovator(settings.AUTH_USER_MODEL):
+    objects = InnovatorManager()
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = settings.AUTH_USER_MODEL.INNOVATOR
+        return super().save(*args, **kwargs)
+
+
+class Investor(settings.AUTH_USER_MODEL):
+    objects = InvestorManager()
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = settings.AUTH_USER_MODEL.Types.INVESTOR
+        return super().save(*args, **kwargs)
--- crowdsourcing/settings.py
+++ crowdsourcing/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'accounts',
+    'phonenumbers',
+    'phonenumber_field',
+    'django_celery_beat',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODE = 'accounts.User'
