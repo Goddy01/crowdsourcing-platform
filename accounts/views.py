64ffbbe6ac062f9b155e464a9180faa674601f82
Toshihiro <adigungodwin2@gmail.com>
2023-02-07 19:18:48 +0100

    added content to innovator_sign_up view

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from . import forms
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from .tokens import account_activation_token
+from django.conf import settings
+from django.core.mail import send_mail
+from .models import UserAccount
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-def sign_in(request):
-    
-    return render(request, 'accounts/sign_in.html')
-
-def sign_up(request):
-
+def innovator_sign_up(request):
+    if request.method == 'POST':
+        signup_form = forms.SignUpForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate your account'
+            message = render_to_string('accounts/activation_request.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = [signup_form.cleaned_data.get('email')]
+            from_email = settings.EMAIL_HOST_USER
+            send_mail(subject, message, from_email, to_email, fail_silently=True)
+            return redirect('accounts:activation_sent')
+    else:
+        signup_form = forms.SignUpForm()
     return render(request, 'accounts/sign_up.html')
+
+def sign_in(request):
+    return render(request, 'accounts/sign_in.html')
