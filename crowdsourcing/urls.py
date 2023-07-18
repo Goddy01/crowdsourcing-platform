6c20f08dbf8090d957c543ca6a6a5c1d5cb19461
Goddy01 <adigungodwin2@gmail.com>
2023-07-18 21:21:00 +0100

    added the urls for password reset

--- crowdsourcing/urls.py
+++ crowdsourcing/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from core import views
 
 urlpatterns = [
@@ -24,6 +25,25 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', 'accounts')), # For social-auth
     path('social-auth/', include('social_django.urls', namespace='social')),
     path('', views.home, name='home'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="password/password_change_done.html"), name="password_change_done"),
+
+    # path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password/password_change.html'), name='password_change'),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), name="password_reset_done"),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name="password_reset_complete"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password/password_reset_form.html', 
+        success_url = reverse_lazy('password_reset_complete'),
+    ), name="password_reset_confirm"),    
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password/password_reset.html',
+        email_template_name='password/password_reset_email.html',
+        subject_template_name='password/password_reset_subject.txt',
+        success_url = reverse_lazy('password_reset_done'),
+    ), name='password_reset'),
 ]
 
 
