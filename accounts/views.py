91ed4405f4607e77c66373e9af16d2b13a52401c
Goddy01 <adigungodwin2@gmail.com>
2023-07-20 14:26:42 +0100

    improved how erros were displayed in the sign_in and sign_op template

--- accounts/views.py
+++ accounts/views.py
@@ -21,56 +21,60 @@ def activation_sent_view(request):
     return render(request, 'accounts/activation_sent.html')
 
 def innovator_sign_up(request):
-    if request.method == 'POST':
-        form = InnovatorSignUpForm(request.POST)
-        print('PRINCE')
-        if form.is_valid():
-            print('GOD IS GOOD')
-            user = form.save(commit=False)
-            user.is_active = False
-            user.date_joined = datetime.now()
-            user.last_login = datetime.now()
-            user.type = "INNOVATOR"
-            user.first_name = user.social_auth.get(provider='linkedin').extra_data['first_name'] or user.social_auth.get(provider='google-oauth2').extra_data['first_name']
-            user.last_name = user.social_auth.get(provider='linkedin').extra_data['last_name'] or user.social_auth.get(provider='google-oauth2').extra_data['last_name']
-            random_number = random.randint(1000, 9999)  # Generate a random 4-digit number
-            user.username = f"{user.social_auth(provider='google-oauth2').extra_data['lastname']}{user.social_auth(provider='google-oauth2').extra_data['firstname']}{random_number}"
-            # try:
-            user.email = user.social_auth(provider='google-oauth2').extra_data['email']
-            # except:
-                # except IntegrityError:
-                # form.add_error('email', 'This email is already registered.')
-            try:
-                UserProfile.objects.filter(email=user.social_auth(provider='google-oauth2').extra_data['email']).exists()
-                print('BROOOSS')
-                return redirect('email-exists')
-            except:
-                form.add_error('email', 'This email is already registered.')
-            #     raise ValueError('This email has already been registered.')
-            # try:
-            #     UserProfile.objects.get(email=user.social_auth(provider='google-oauth1').extra_data['email']).exists() or UserProfile.objects.get(email=user.email)
-            # except ValidationError as e:
-            #     return HttpResponse('Email already exists')
-            user.save()
-            current_site = get_current_site(request)
-            subject = 'Activate your account'
-            message = render_to_string('accounts/activation_request.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = [form.cleaned_data.get('email')]
-            from_email = settings.EMAIL_HOST_USER
-            send_mail(subject, message, from_email, to_email, fail_silently=True)
-            return redirect('accounts:activation_sent')
+    context = {}
+    if not request.META.get('HTTP_REFERER'):
+        context['email_exists'] = 'A Google account is already linked with the email provided.'
     else:
-        form = InnovatorSignUpForm()
+        if request.method == 'POST':
+            form = InnovatorSignUpForm(request.POST)
+            print('PRINCE')
+            if form.is_valid():
+                print('GOD IS GOOD')
+                user = form.save(commit=False)
+                user.is_active = False
+                user.date_joined = datetime.now()
+                user.last_login = datetime.now()
+                user.type = "INNOVATOR"
+                user.first_name = user.social_auth.get(provider='linkedin').extra_data['first_name'] or user.social_auth.get(provider='google-oauth2').extra_data['first_name']
+                user.last_name = user.social_auth.get(provider='linkedin').extra_data['last_name'] or user.social_auth.get(provider='google-oauth2').extra_data['last_name']
+                random_number = random.randint(1000, 9999)  # Generate a random 4-digit number
+                user.username = f"{user.social_auth(provider='google-oauth2').extra_data['lastname']}{user.social_auth(provider='google-oauth2').extra_data['firstname']}{random_number}"
+                # try:
+                user.email = user.social_auth(provider='google-oauth2').extra_data['email']
+                # except:
+                    # except IntegrityError:
+                    # form.add_error('email', 'This email is already registered.')
+                # try:
+                #     UserProfile.objects.filter(email=user.social_auth(provider='google-oauth2').extra_data['email']).exists()
+                #     print('BROOOSS')
+                #     return redirect('email-exists')
+                # except:
+                #     form.add_error('email', 'This email is already registered.')
+                #     raise ValueError('This email has already been registered.')
+                # try:
+                #     UserProfile.objects.get(email=user.social_auth(provider='google-oauth1').extra_data['email']).exists() or UserProfile.objects.get(email=user.email)
+                # except ValidationError as e:
+                #     return HttpResponse('Email already exists')
+                user.save()
+                current_site = get_current_site(request)
+                subject = 'Activate your account'
+                message = render_to_string('accounts/activation_request.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = [form.cleaned_data.get('email')]
+                from_email = settings.EMAIL_HOST_USER
+                send_mail(subject, message, from_email, to_email, fail_silently=True)
+                return redirect('accounts:activation_sent')
+        else:
+            form = InnovatorSignUpForm()
+        context['innovator_signup_form'] = form
     return render(request, 'accounts/sign_up.html', context={'innovator_signup_form': form})
 
 def innovator_sign_in(request):
     context = {}
-    print('PRE: ', request.META.get('HTTP_REFERER'))
     if not request.META.get('HTTP_REFERER'):
         context['email_exists'] = 'A Google account is already linked with the email provided.'
     else:
