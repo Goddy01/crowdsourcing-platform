7018105c5da1e1f4006a43f01d7af206d422e87f
Goddy01 <adigungodwin2@gmail.com>
2023-07-21 17:43:19 +0100

    reg'd Contributor in its admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BaseUser
+from .models import BaseUser, Contributor
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 class AccountAdmin(UserAdmin):
@@ -8,3 +8,10 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 admin.site.register(BaseUser, AccountAdmin)
+
+# class ContributorAdmin(UserAdmin):
+#     readonly_fields = ('date_joined', 'last_login')
+#     filter_horizontal = ()
+#     list_filter = ()
+#     fieldsets = ()
+admin.site.register(Contributor)
