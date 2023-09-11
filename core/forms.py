07456cc4e769758dd0a25027ab3afbbdfd94f8b0
Goddy01 <adigungodwin2@gmail.com>
2023-09-11 15:33:52 +0100

    updated the add-project page

--- core/forms.py
+++ core/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class CreateProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['name', 'motto', 'description', 'target', 'expected_return', 'term_months', 'country', 'investment_deadline', 'image_1', 'image_2', 'image_3', 'video', 'business_type']
+        fields = ['name', 'motto', 'description', 'target', 'expected_return', 'term_months', 'country', 'investment_deadline', 'image_1', 'image_2', 'image_3', 'video', 'business_type', 'innovator_user_agreement']
--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Project
+from django_countries import countries
 # Create your views here.
 def home(request):
     
     return render(request, 'index.html')
 
 def add_project(request):
-    
-    return render(request, 'core/add-project.html')
+
+    return render(request, 'core/add-project.html', {'countries': countries})
