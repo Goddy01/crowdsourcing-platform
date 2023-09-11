fbf571c4d66bf81574993041e5eb66ec90ded559
Goddy01 <adigungodwin2@gmail.com>
2023-09-11 11:33:19 +0100

    added add_project basic view

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, 'index.html')
+
+def add_project(request):
+    
+    return render(request, 'core/add-project.html')
