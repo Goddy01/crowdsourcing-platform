05c46c1523b7f938e434a9d86f8bf33bc76ce5c3
Goddy01 <adigungodwin2@gmail.com>
2023-09-16 10:53:52 +0100

    added CreateInnovationForm

--- core/forms.py
+++ core/forms.py
@@ -66,3 +66,7 @@ class CreateProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'motto', 'description', 'target', 'expected_return', 'term_months', 'country', 'investment_deadline', 'image_1', 'image_2', 'image_3', 'video', 'business_type', 'innovator_user_agreement']
+
+class CreateInnovationForm(forms.ModelForm):
+    class Meta:
+        model = ['title', 'description', 'image', 'category', 'reward']
