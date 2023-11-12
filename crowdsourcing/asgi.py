148104825c648a0df26e24ce8a96ce5d26a93306
Goddy01 <adigungodwin2@gmail.com>
2023-11-12 19:54:53 +0100

    modified asgi file

--- crowdsourcing/asgi.py
+++ crowdsourcing/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for crowdsourcing project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+from chat import routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowdsourcing.settings')
 
-application = get_asgi_application()
+django_asgi_application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_application,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        )
+    }
+)
