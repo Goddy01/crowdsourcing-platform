60a0b39f00376bb35af02f94f2af4d11fd5c2c7a
Goddy01 <adigungodwin2@gmail.com>
2023-11-14 14:28:39 +0100

    added room view

--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'chat/chat2.html')
+
+def room(request, room_name):
+    return render(request, "chat/room.html", {"room_name": room_name})
