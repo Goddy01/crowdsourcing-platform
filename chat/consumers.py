b5ededa3058080e5589b36f07a2635148d3cb1e5
Goddy01 <adigungodwin2@gmail.com>
2023-11-14 07:57:16 +0100

    added a main consumer to consumers.py

---
+++ chat/consumers.py
@@ -0,0 +1,13 @@
+from ably import AblyRealtime
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+async def main():
+    ably = AblyRealtime(os.environ.get('ROOT_API_KEY'))
+    await ably.connection.once_async('connected')
+    print('Connected to Ably')
+
+
+asyncio.run(main())
