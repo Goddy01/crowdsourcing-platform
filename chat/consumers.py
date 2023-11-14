bb57f037690c29d1d34c318919e118121750b4c0
Goddy01 <adigungodwin2@gmail.com>
2023-11-14 14:51:45 +0100

    updated consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,15 +1,15 @@
-from ably import AblyRealtime
-import asyncio
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import json
+from channels.generic.websocket import WebsocketConsumer
 
 
-async def main():
-    ably = AblyRealtime(os.environ.get('ROOT_API_KEY'))
-    await ably.connection.once_async('connected')
-    print('Connected to Ably')
-
-
-asyncio.run(main())
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+    
+    def disconnect(self, close_code):
+        pass
+    
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        self.send(text_data=json.dumps({"message": message}))
