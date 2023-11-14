461dee0f10b82c5ea7f65f6a29f9f672ff08b4fa
Goddy01 <adigungodwin2@gmail.com>
2023-11-14 14:07:36 +0100

    updated project's asgi file

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
 async def main():
     ably = AblyRealtime(os.environ.get('ROOT_API_KEY'))
     await ably.connection.once_async('connected')
--- chat/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def chat_home(request):
-    return render(request, 'chat/chat.html')
