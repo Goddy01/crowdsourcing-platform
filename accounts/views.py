b7d3468eec4f45877aaffd6a2dbd2819f2355d35
Goddy01 <adigungodwin2@gmail.com>
2023-11-16 21:47:12 +0100

    improved accept/decline conn request

--- accounts/views.py
+++ accounts/views.py
@@ -703,6 +703,7 @@ def send_connection_request(request, recipient_pk):
 def friend_requests(request):
     friend_requests = ConnectionRequest.objects.filter(
         recipient__user__pk=request.user.pk,
+        
     )
     return render(request, 'accounts/friend_requests.html', {'friend_requests': friend_requests})
 
