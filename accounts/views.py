a4d72180ce808d59a667af31aca01c57847e182a
Goddy01 <adigungodwin2@gmail.com>
2023-11-16 12:16:56 +0100

    updated friend request feature

--- accounts/views.py
+++ accounts/views.py
@@ -700,4 +700,4 @@ def friend_requests(request):
     friend_requests = ConnectionRequest.objects.filter(
         recipient__user__pk=request.user.pk,
     )
-    return render(request, 'accounts/friend_requests', {'friend_requests': friend_requests})
+    return render(request, 'accounts/friend_requests.html', {'friend_requests': friend_requests})
