d99ca2610fb4bd34520e22634d17393e6bd31a1b
Goddy01 <adigungodwin2@gmail.com>
2023-12-02 14:38:40 +0100

    added a proxy websocket routing for ws/chat/groups/

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/friends/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/groups/$", consumers.ChatConsumer.as_asgi()),
 
 ]
