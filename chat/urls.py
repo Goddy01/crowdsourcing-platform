bc23dc163fe83af513eea24a37b9b30047718adf
Goddy01 <adigungodwin2@gmail.com>
2023-12-12 22:37:36 +0100

    added its urls

--- chat/urls.py
+++ chat/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('', views.room, name='chat'),
     path('<sender>/send-file/<recipient>/', views.send_file_message, name='send_file_msg'),
     path('<parent_message>/<sender>/send-file/<recipient>/', views.send_file_message, name='send_file_msg_with_parent'),
+
+    path('group/<sender>/send-file', views.send_group_file_message, name='send_group_file_msg'),
+    path('group/<parent_message>/<sender>/send-file', views.send_group_file_message, name='send_group_file_msg_with_parent'),
     path('<group_pk>/get-group-members', views.get_group_members, name='get_group_members'),
 
 ]
