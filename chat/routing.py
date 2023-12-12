fe603ee0f62a3804a122f29d0ff662b78f75d568
Goddy01 <adigungodwin2@gmail.com>
2023-12-12 17:31:18 +0100

    added GroupChatConsumer routing

--- chat/routing.py
+++ chat/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/friends/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/groups/$", consumers.GroupChatConsumer.as_asgi()),
 ]
