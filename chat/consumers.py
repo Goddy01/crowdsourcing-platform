e76e7a2d938e45df2406e022d632378ee4c5e851
Goddy01 <adigungodwin2@gmail.com>
2023-12-12 16:53:09 +0100

    added necessary consumers for conn to GroupChatConsumer class

--- chat/consumers.py
+++ chat/consumers.py
@@ -294,3 +294,27 @@ class GroupChatConsumer(WebsocketConsumer):
             'tagged_msg_content': tagged_content,
             'tagged_msg_file_content': tagged_file_content,
         }
+    def connect(self):
+        self.room_group_name = 'chat_room'
+        
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync( self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
+
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+        self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
+    def chat_message(self, event):
+        message = event["message"]
+        self.send(text_data=json.dumps(message))
