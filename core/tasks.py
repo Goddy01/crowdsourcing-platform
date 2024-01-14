8cbb1ab9e786e5cac09085d2f60c6f279d54fcdb
Goddy01 <adigungodwin2@gmail.com>
2024-01-14 13:10:40 +0100

    added send_project_approval_status_task celery task

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from .views import send_funding_completed_email, send_milestone_email
+from .views import send_funding_completed_email, send_milestone_email, send_project_approval_status
 from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
@@ -21,3 +21,10 @@ def send_milestone_email_task(**kwargs):
     type = kwargs.get('type')
     logger.info('MILESTONE EMAIL SENT')
     return send_milestone_email(investment_pk=investment_pk, current_site=current_site, milestone_pk=milestone_pk, type=type)
+
+
+@shared_task
+def send_project_approval_status_task(**kwargs):
+    investment_pk = kwargs.get('investment_pk')
+    logger.info('PROJECT APPROVAL STATUS EMAIL SENT')
+    return send_project_approval_status(investment_pk)
