fb7091a18027e386f2f4b0d27169ef1b7c32d57d
Goddy01 <adigungodwin2@gmail.com>
2024-01-15 15:16:01 +0100

    added pay_investors_task celery task

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from .views import send_funding_completed_email, send_milestone_email, send_project_approval_status
+from .views import send_funding_completed_email, send_milestone_email, send_project_approval_status, pay_investors
 from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
@@ -28,3 +28,10 @@ def send_project_approval_status_task(**kwargs):
     investment_pk = kwargs.get('investment_pk')
     logger.info('PROJECT APPROVAL STATUS EMAIL SENT')
     return send_project_approval_status(investment_pk)
+
+@shared_task
+def pay_investors_task(**kwargs):
+    request = kwargs.get('request')
+    investment_pk = kwargs.get('investment_pk')
+    logger.info('PAYMENT OF INVESTORS INITIATED')
+    return pay_investors(request=request, investment_pk=investment_pk)
