1401dbe375b8c9aa0748df8b8beef6d8aafded5a
Goddy01 <adigungodwin2@gmail.com>
2024-02-10 09:46:53 +0100

    updated

--- crowdsourcing/settings.py
+++ crowdsourcing/settings.py
@@ -18,7 +18,12 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+
+if DEBUG:
+    load_dotenv()
+else:
+    load_dotenv(".env.sample")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +33,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # 0 => False
 # 1 => True
-DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS").split(',')
 # CSRF_TRUSTED_ORIGINS = ['*']
